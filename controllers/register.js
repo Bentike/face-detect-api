cde4586acfd5a6a01abf2afa1ce9f6a1af293dd6
ajadi yusuf <yusufajadi93@gmail.com>
2020-10-07 16:59:22 +0100

    fixed handleRegister function

--- controllers/register.js
+++ controllers/register.js
@@ -1,30 +1,45 @@
+const { response } = require("express");
+
 const handleRegister = (req, res, db, bcrypt) => {
 	const {name, email, password} = req.body;
+	const userExist = () => {
+		res.status(400).json('user already exist')
+	};
 	if(name.length && email.length && password.length > 0){
-	    const hash = bcrypt.hashSync(password);
-		db.transaction(trx => {
-			trx.insert({
-				hash: hash,
-				email: email
-			})
-			 .into('login')
-			 .returning('email')
-			 .then(loginEmail => {
-	          return trx('users')
-				  .returning('*')
-				  .insert({
-					name: name,
-					email: loginEmail[0],
-					joined: new Date()
+		const hash = bcrypt.hashSync(password);
+		db.select('*').from('users')
+		.where('email', '=', email)
+		.then(response => {
+			if(!response[0]){
+				db.transaction(trx => {
+					trx.insert({
+						hash: hash,
+						email: email
+					})
+					 .into('login')
+					 .returning('email')
+					 .then(loginEmail => {
+					  return trx('users')
+						  .returning('*')
+						  .insert({
+							name: name,
+							email: loginEmail[0],
+							joined: new Date()
+						})
+					  .then(user => {
+					   res.json(user[0]);
+					   })
+					})
+					 .then(trx.commit)
+					 .catch(trx.rollback)
 				})
-			  .then(user => {
-		       res.json(user[0]);
-			   })
-			})
-			 .then(trx.commit)
-			 .catch(trx.rollback)
+				  .catch(err => res.status(400).json('unable to register'));
+			}else{
+				return userExist();
+			}
 		})
-		  .catch(err => res.status(400).json('unable to register'));
+		.catch(err => console.log(err));
+		
 	}
 }
 
