b9e8ef82ec13757bc4b1df13d0d98ad23d0f0b7d
ajadi yusuf <yusufajadi93@gmail.com>
2020-10-15 16:59:57 +0100

    working on db connection

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const register = require('./controllers/register');
 
 const db = knex({
   client: 'pg',
-  connection: {
-	  host: process.env.DATABASE_URL,
-	  ssl: true
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
