9dbae7d74b1a99559b00cd860c95cccead86a137
ajadi yusuf <yusufajadi93@gmail.com>
2020-10-20 15:56:04 +0100

    added knex

--- server.js
+++ server.js
@@ -2,26 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-//const knex = require('knex');
+const knex = require('knex');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const register = require('./controllers/register');
-const { Pool } = require('pg');
-const db = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false
-	}
-});
 
-// const db = knex({
-//   client: 'pg',
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const db = knex({
+  client: 'pg',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
